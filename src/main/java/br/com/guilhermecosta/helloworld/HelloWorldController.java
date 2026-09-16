729178875d54175a47c240328b3a8aef468c79b8
Guilherme Costa <guilerme18@exemplo.com>
2026-09-16 19:35:44 -0300

    Adiciona endpoint de soma

--- src/main/java/br/com/guilhermecosta/helloworld/HelloWorldController.java
+++ src/main/java/br/com/guilhermecosta/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package br.com.guilhermecosta.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/some")
+    public int somar(@RequestParam int numero1, @RequestParam int numero2) {
+        return numero1 + numero2;
     }
 }
